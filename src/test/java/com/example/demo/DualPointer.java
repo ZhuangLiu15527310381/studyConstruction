5dba543cf02c76a95c2a3ed7e9fffb9fdb9a0af8
liuzhuang10 <liuzhuang10@hikvision.com.cn>
2024-06-20 16:49:06 +0800

    [ADD]字符串右旋指定位置

--- src/test/java/com/example/demo/DualPointer.java
+++ src/test/java/com/example/demo/DualPointer.java
@@ -147,7 +147,7 @@ public class DualPointer {
     @Test
     public void testWordsReverse(){
 
-        String s = reverseWords("a good   example");
+        String s = reverseWords("a good  example   ");
         System.out.println(s);
 
 
@@ -215,5 +215,58 @@ public class DualPointer {
         }
         return stringBuilder;
     }
+
+
+    /**
+     * 字符串的右旋转操作是把字符串尾部的若干个字符转移到字符串的前面。
+     * 给定一个字符串 s 和一个正整数 k，请编写一个函数，将字符串中的后面 k 个字符移到字符串的前面，实现字符串的右旋转操作。
+     *
+     * 例如，对于输入字符串 "abcdefg" 和整数 2，函数应该将其转换为 "fgabcde"。
+     *
+     * 输入：输入共包含两行，第一行为一个正整数 k，代表右旋转的位数。第二行为字符串 s，代表需要旋转的字符串。
+     *
+     * 输出：输出共一行，为进行了右旋转操作后的字符串。
+     */
+    @Test
+    public void testRight(){
+
+        String abcdefg = rightReverse("abcdefg", 2);
+        System.out.println(abcdefg);
+
+    }
+
+    public String rightReverse(String inputString , int k){
+        if (Objects.isNull(inputString)){
+            return inputString;
+        }
+        if (k > inputString.length()){
+            return inputString;
+        }
+        char[] charArray = inputString.toCharArray();
+        int startIndex=0;
+        int endIndex = charArray.length - k;
+        StringBuilder sb = new StringBuilder();
+        while (endIndex < charArray.length){
+            sb.append(charArray[endIndex]);
+            endIndex++;
+        }
+        while (startIndex < charArray.length-k){
+            sb.append(charArray[startIndex]);
+            startIndex++;
+        }
+
+        return sb.toString();
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
